98c0a9e273c278c56c6cf0425ae95987a65a5176
Kevin Glick <kevin.n.glick@gmail.com>
2016-11-10 21:07:08 -0500

    add function interpolation

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,15 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Hello World!'
+    title: 'Hello World!',
+    hello: 'Hi! Welcome!'
   },
   methods: {
     changeTitle: function(event) {
       this.title = event.target.value;
+    },
+    sayHello: function() {
+      return this.hello;
     }
   }
 });
