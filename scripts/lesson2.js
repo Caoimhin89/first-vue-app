bd29fd1db0dd6efacbfc8c1e05f486a54ef1b3e6
Kevin Glick <kevin.n.glick@gmail.com>
2016-11-12 13:12:50 -0500

    passing arguments with events

--- scripts/lesson2.js
+++ scripts/lesson2.js
@@ -6,8 +6,8 @@ new Vue({
     counter: 0
   },
   methods: {
-    increase: function() {
-      this.counter++;
+    increase: function(step) {
+      this.counter += step;
     },
     updateCoordinates: function(event) {
       this.x = event.clientX;
