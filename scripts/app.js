8c37321a18979a96fb6e64f297de5c106d00e381
Kevin Glick <kevin.n.glick@gmail.com>
2016-11-11 19:20:25 -0500

    add re-render block

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,8 @@ new Vue({
   data: {
     title: 'Hello World! Try out some string interpolation! Type something in the text box.',
     hello: 'Hi! Do you like function interpolation?',
-    link: 'http://wikipedia.org/'
+    link: 'http://wikipedia.org/',
+    text: "I can prevent re-rendering with Vue's 'v-once' directive. I've attached it to the third list element here. Go ahead... try to change it."
   },
   methods: {
     changeTitle: function(event) {
@@ -11,6 +12,9 @@ new Vue({
     },
     sayHello: function() {
       return this.hello;
+    },
+    changeText: function(event) {
+      this.text = event.target.value;
     }
   }
 });
