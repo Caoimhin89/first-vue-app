eb2f6d2b2ce757232a62f7ed3dd25d349a1ca1e8
Kevin Glick <kevin.n.glick@gmail.com>
2016-11-13 18:24:41 -0500

    complete unit 3 exercises

--- scripts/exercises3.js
+++ scripts/exercises3.js
@@ -1,15 +1,33 @@
 new Vue({
   el: '#exercises3',
   data: {
-
+    value: 0,
+    duration: 0
   },
   computed: {
-
+    result: function() {
+      return this.value !== 37? 'Not there yet!' : 'Done!';
+    },
+    message: function() {
+      return this.value > 37? `Woops! You went over 37! Looks like you need to
+       reset and try again.`: 'Try to get to 37 without going over.';
+    }
   },
   watch: {
-
+    result: function() {
+      var vm = this;
+      setTimeout(function() {
+        vm.value = 0;
+      }, vm.duration);
+    }
   },
   methods: {
-
+    setDuration: function(event) {
+      this.duration = event.target.value;
+    },
+    resetValues: function() {
+      this.value = 0;
+      this.duration = 0;
+    }
   }
 });
