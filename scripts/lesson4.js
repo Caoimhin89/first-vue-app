594400d76537789875cdabc90e23d3a7108a5f6b
Kevin Glick <kevin.n.glick@gmail.com>
2016-11-13 21:16:15 -0500

    complete lesson 4

--- scripts/lesson4.js
+++ scripts/lesson4.js
@@ -1,7 +1,24 @@
 new Vue({
   el: '#lesson4',
   data: {
-    attachRed: false
+    attachRed: false,
+    color: 'green',
+    width: 100,
+    height: 100
+  },
+  computed: {
+    divClasses: function() {
+      return {
+        red: this.attachRed,
+        blue: !this.attachRed
+      };
+    },
+    myStyle: function() {
+      return {
+        backgroundColor: this.color,
+        width: this.width + 'px'
+      }
+    }
   },
   methods: {
 
